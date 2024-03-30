0d717b0d32fbe0d0a75b40198e3ab113959e0df2
Piyush Bansal <pbansal@thesummitgrp.com>
2024-03-30 19:38:49 +0530

    Emotion Prediction

--- app.py
+++ app.py
@@ -13,7 +13,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+pipe_lr = joblib.load(open("emotion_classifier_pipe_lr.pkl", "rb"))
+
+def predict_emotions(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
+
+def get_prediction_proba(docx):
+    results = pipe_lr.predict_proba([docx])
+    return results
+
+
+
+
 @app.post("/predict")
-async def root(text: str):
+async def root(raw_text: str):
+    prediction = predict_emotions(raw_text)
+    probability = get_prediction_proba(raw_text)
+
+    emotions = ['anger', 'disgust', 'fear', 'joy', 'neutral', 'sadness', 'shame', 'surprise']
+    probability_dict = {emotion: prob for emotion, prob in zip(emotions, probability[0])}
 
     return JSONResponse(content={})
